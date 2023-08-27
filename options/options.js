c1e0c69acd2cff2840f7d2d8a8334b86937d8491
Crowdbotics <team@crowdbotics.com>
2023-08-27 16:21:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sacred-society-43104",
+  url: "https://sacred-society-43104.botics.co",
+  api: "https://sacred-society-43104.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
